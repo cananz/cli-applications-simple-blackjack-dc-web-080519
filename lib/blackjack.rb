e15c901fd56a59bcb030c2c5894f566a225f79eb
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-08-02 17:52:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,7 +6,7 @@ def deal_card
   return rand(1..11)
 end
 
-def display_card_total (total)
+def display_card_total(total)
   puts "Your cards add up to #{total}"
 end
 
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts 
 end
 
 def initial_round
