c34a57bf7255ff03a4ea0fc140bc6b740ce6a08c
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-08-02 18:43:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,11 +55,12 @@ end
 def runner
   welcome
   current_hand = initial_round
-  if current_hand <= 21
+if current_hand <= 21
     current_hand = hit?(current_hand)
     display_card_total(current_hand)
   end
-    
-
+else
+  end_game
+end
 end
     
